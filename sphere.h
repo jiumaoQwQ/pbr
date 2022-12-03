6379cbb7ef9646753601f63d3fe65f6bc9e903e5
MihiroQwQ <714962994@qq.com>
2022-12-03 20:10:16 +0800

    123

---
+++ sphere.h
@@ -0,0 +1,14 @@
+#include <glm/vec3.hpp>
+
+class Sphere
+{
+public:
+    Sphere(glm::vec3 _pos = {0, 0, 0}, float _radius = 1.0f) : pos(_pos), radius(_radius){};
+    void draw()
+    {
+    }
+
+private:
+    glm::vec3 pos;
+    float radius;
+};
